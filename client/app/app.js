3ec3429d9b544542ac5bdca06cfb086931dd3e97
Ben Stone <benjamin.v.stone@gmail.com>
2015-07-11 17:18:55 -0700

    Add initial routing and homepage markup

--- client/app/app.js
+++ client/app/app.js
@@ -2,5 +2,25 @@ angular.module('teamHistory', [
   'ui.router'
 ])
 
-
+.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', 
+  function($stateProvider, $urlRouterProvider, $locationProvider) {
+    
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+    
+    $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: 'app/views/home.html'
+      })
+      .state('stats', {
+        url: '/stats/:team',
+        template: '<h1>Now on the stats page</h1>'       
+      });
+  
+  }])
 ;
