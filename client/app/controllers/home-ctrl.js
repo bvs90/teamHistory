8be826f320c69072e815b8ced895b893b90f849e
Ben Stone <benjamin.v.stone@gmail.com>
2015-07-30 21:57:41 -0700

    Add team manifest to homeCtrl, Clean up home view to use ng-repeat

--- client/app/controllers/home-ctrl.js
+++ client/app/controllers/home-ctrl.js
@@ -8,5 +8,38 @@ angular.module('srcModule')
   homeCtrl.selectTeam = function(team) {
     homeCtrl.setTeam(team);
   };
+  
+  homeCtrl.teamList = [
+    [
+      { teamName: 'Arsenal', teamShort: 'ARS'},
+      { teamName: 'Aston_Villa', teamShort: 'ASV'},
+      { teamName: 'Bournemouth', teamShort: 'BOU'},
+      { teamName: 'Chelsea', teamShort: 'CHE'},
+      { teamName: 'Crystal_Palace', teamShort: 'CPA'},
+      { teamName: 'Everton', teamShort: 'EVT'},
+      { teamName: 'Leicester_City', teamShort: 'LEI'},
+      { teamName: 'Liverpool', teamShort: 'LIV'}
+    ],
+    [
+      { teamName: 'Manchester_City', teamShort: 'MCY'},
+      { teamName: 'Manchester_United', teamShort: 'MUN'}
+    ],
+    [
+      { teamName: 'Newcastle_United', teamShort: 'NEW'},
+      { teamName: 'Norwich_City', teamShort: 'NOR'}      
+    ],
+    [
+      { teamName: 'Southampton', teamShort: 'SOU'},
+      { teamName: 'Stoke_City', teamShort: 'STK'},
+      { teamName: 'Sunderland', teamShort: 'SUN'},
+      { teamName: 'Swansea_City', teamShort: 'SWA'},
+      { teamName: 'Tottenham_Hotspur', teamShort: 'TOT'},
+      { teamName: 'Watford', teamShort: 'WAT'},
+      { teamName: 'West_Bromwich_Albion', teamShort: 'WBA'},
+      { teamName: 'West_Ham_United', teamShort: 'WHU'}      
+    ]
+  ];
+  
+  
 }])
 ;
